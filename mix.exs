751a32a6c3a946d5877775db422d4f2f9890340e
1cbyc <isaacnsisong@gmail.com>
2023-10-17 02:26:22 +0100

    adding Pow for user auth

--- mix.exs
+++ mix.exs
@@ -51,6 +51,10 @@ defmodule TodoList.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"}
+      {:pow, "~> 1.0"},
+      {:comeonin, "~> 5.1"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.10"},
     ]
   end
 
